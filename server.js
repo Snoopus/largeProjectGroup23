c6f2b8d9fcdba344c260800c9363d090e6eb24cd
Snoopus <iantheturtle04@gmail.com>
2025-10-09 05:20:13 +0000

    first commit

---
+++ server.js
@@ -0,0 +1,150 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+// const MongoClient = require('mongodb').MongoClient;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+//const url = '';
+//Go to drivers and get connection string for MongoDB
+//You have to set incoming ip address to all, 0.0.0.0/0
+
+// const client = new MongoClient(url);
+const url = process.env.MONGO_URL;
+const client = new MongoClient(url, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+//client.connect();
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.use(cors());
+app.use(express.json());
+app.use((req, res, next) =>
+{
+    res.setHeader('Access-Control-Allow-Origin', '*'); 
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET, POST, PATCH, DELETE, OPTIONS'
+    );
+    next();
+});
+//app.listen(5000); // start Node + Express server on port 5000
+const server = app.listen(5000, () => {
+  console.log('Server running on port 5000');
+});
+
+app.post('/api/addcard', async (req, res, next) =>
+{
+  // incoming: userId, color
+  // outgoing: error
+	
+  const { userId, card } = req.body;
+
+  const newCard = {Card:card,UserId:userId};
+  var error = '';
+
+  try
+  {
+    const db = client.db('Project');
+    const result = await db.collection('Cards').insertOne(newCard);
+  }
+  catch(e)
+  {
+    error = e.toString();
+  }
+
+
+  var ret = { error: error };
+  res.status(200).json(ret);
+});
+
+
+app.post('/api/login', async (req, res, next) => 
+{
+  // incoming: login, password
+  // outgoing: id, firstName, lastName, error
+	
+ var error = '';
+
+  const { login, password } = req.body;
+
+  const db = client.db('Project');
+  const results = await db.collection('Users').find({login:login,password:password}).toArray();
+
+  var id = -1;
+  var fn = '';
+  var ln = '';
+
+  if( results.length > 0 )
+  {
+    id = results[0].UserID;
+    fn = results[0].FirstName;
+    ln = results[0].LastName;
+  }
+
+  var ret = { id:id, firstName:fn, lastName:ln, error:''};
+  res.status(200).json(ret);
+});
+
+
+app.post('/api/searchcards', async (req, res, next) => 
+{
+  // incoming: userId, search
+  // outgoing: results[], error
+
+  var error = '';
+
+  const { userId, search } = req.body;
+
+  var _search = search.trim();
+  
+  const db = client.db('Project');
+  const results = await db.collection('Cards').find({"Card":{$regex:_search+'.*', $options:'i'}}).toArray();
+  
+  var _ret = [];
+  for( var i=0; i<results.length; i++ )
+  {
+    _ret.push( results[i].Card );
+  }
+  
+  var ret = {results:_ret, error:error};
+  res.status(200).json(ret);
+});
+
+process.on('SIGTERM', () => {
+  console.log('Received SIGTERM. Shutting down server...');
+  client.close();
+  server.close(() => {
+    console.log('Server closed');
+    process.exit(0);
+  });
+});
+
+process.on('SIGINT', () => {
+  console.log('Received SIGINT. Shutting down server...');
+  client.close();
+  server.close(() => {
+    console.log('Server closed');
+    process.exit(0);
+  });
+});
+
+
