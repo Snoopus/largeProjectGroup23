59bff9b15865e3e07ed2fedb89708fcf33798436
Snoopus <iantheturtle04@gmail.com>
2025-10-23 02:32:25 -0400

    make server.js listen everywhere

--- server.js
+++ server.js
@@ -47,8 +47,9 @@ app.use((req, res, next) =>
     next();
 });
 //app.listen(5000); // start Node + Express server on port 5000
-const server = app.listen(5000, () => {
-  console.log('Server running on port 5000');
+const PORT = process.env.PORT || 5000;
+const server = app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT} and accessible from all network interfaces`);
 });
 
 app.post('/api/addcard', async (req, res, next) =>
